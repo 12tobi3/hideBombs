e518c8f805acd2f2d7e4bb8227d28e196b8d797b
Tobias Schultze <Tobi@Tobiass-MBP.fritz.box>
2016-03-29 20:09:52 +0200

    HideBombs

--- HideBombs/ViewController.swift
+++ HideBombs/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
+    
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        redBomb.hidden = true
     }
 
 
